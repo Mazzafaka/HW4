1030b10f08cd39c8ebd8c04fdf6ac3f9f7b96e5c
Zack <1984odina.gmail.com>
2023-07-18 21:43:57 +0600

    Added all

---
+++ DID_1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+void Main(){
+     Console.Write("Введите число A: ");
+        int A = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите число B: ");
+        int B = int.Parse(Console.ReadLine());
+
+        int result = Power(A, B);
+        Console.WriteLine($"{A} в степени {B} равно {result}");
+}
+
+int Power(int baseNumber, int exponent)
+    {
+        int result = 1;
+        for (int i = 0; i < exponent; i++)
+        {
+            result *= baseNumber;
+        }
+        return result;
+    }
+    
+
+Main();
---
+++ DID_2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+void Main()
+    {
+        Console.Write("Введите число: ");
+        int number = int.Parse(Console.ReadLine());
+
+        int result = SumOfDigits(number);
+        Console.WriteLine($"Сумма цифр числа {number} равна {result}");
+    }
+
+    int SumOfDigits(int number)
+    {
+        int total = 0;
+        foreach (char digitChar in number.ToString())
+        {
+            int digit = int.Parse(digitChar.ToString());
+            total += digit;
+        }
+        return total;
+    }
+
+
+Main();
